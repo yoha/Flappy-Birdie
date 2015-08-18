712d2b1a082fa0383725418d082f93d416a80b51
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-08-18 17:15:46 +0700

    Setup ground's and skyline's properties including their endless animation. Refactored custom methods.

--- Flappy Birdie/GameScene.swift
+++ Flappy Birdie/GameScene.swift
@@ -10,26 +10,40 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    //**************************
     // MARK: - Stored Properties
+    //**************************
     
     var birdie: SKSpriteNode!
     var skyColor = SKColor()
     
     let gravityValue: CGFloat = -5.0
+    let spriteSizeScale: CGFloat = 2.0
     
+    //*************************
     // MARK: - Methods Override
+    //*************************
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
-        // Physics
+        //**************
+        // MARK: Physics
+        //**************
+        
         self.physicsWorld.gravity = CGVectorMake(0.0, self.gravityValue)
         
-        // Sky
+        //**********
+        // MARK: Sky
+        //**********
+        
         self.skyColor = UIColor(red: 113.0/255.0, green: 197.0/255.0, blue: 207.0/255.0, alpha: 1.0)
         self.backgroundColor = self.skyColor
         
-        // Birdie
+        //*************
+        // MARK: Birdie
+        //*************
+        
         let birdieTexture1 = SKTexture(imageNamed: "birdie1")
         birdieTexture1.filteringMode = SKTextureFilteringMode.Nearest
         let birdieTexture2 = SKTexture(imageNamed: "birdie2")
@@ -39,16 +53,26 @@ class GameScene: SKScene {
         let repeatBirdieFlappingAnimationForever = SKAction.repeatActionForever(birdieFlappingAnimation)
         
         self.birdie = SKSpriteNode(texture: birdieTexture1)
-        self.birdie.setScale(2.0)
+        self.birdie.setScale(self.spriteSizeScale)
         self.birdie.position = CGPointMake(self.frame.size.width / 2.5, CGRectGetMidY(self.frame))
         self.birdie.runAction(repeatBirdieFlappingAnimationForever)
         self.addChild(self.birdie)
         
-        // Ground
-//        let groundTexture = SKTexture(imageNamed: "ground")
-//        groundTexture.filteringMode = SKTextureFilteringMode.Nearest
-//        for index in 0 ..<
+        //*************
+        // MARK: Ground
+        //*************
         
+        let groundTexture = SKTexture(imageNamed: "ground")
+        groundTexture.filteringMode = SKTextureFilteringMode.Nearest
+        self.generateContinuousSpriteNodes(groundTexture, action: self.animateContinuousTextures(groundTexture, period: 0.02))
+        
+        //**************
+        // MARK: Skyline
+        //**************
+        
+        let skylineTexture = SKTexture(imageNamed: "skyline")
+        skylineTexture.filteringMode = SKTextureFilteringMode.Nearest
+        self.generateContinuousSpriteNodes(skylineTexture, action: self.animateContinuousTextures(skylineTexture, period: 0.1), groundTextureHeight: groundTexture.size().height)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -59,4 +83,33 @@ class GameScene: SKScene {
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    //***********************
+    // MARK: - Custom Methods
+    //***********************
+
+    func generateContinuousSpriteNodes(texture: SKTexture, action: SKAction, groundTextureHeight: CGFloat = 0) {
+        /**
+        print(self.frame.size.width)
+        print(texture.size().width)
+        print(texture.size().height)
+        print(2 + Int(self.frame.size.width / (texture.size().width * 2)))
+        print("---")
+        **/
+        for index in 0 ..< 2 + Int(self.frame.size.width / (texture.size().width * 2)) {
+            let spriteNode = SKSpriteNode(texture: texture)
+            spriteNode.setScale(self.spriteSizeScale)
+            spriteNode.position = CGPointMake(CGFloat(index) * spriteNode.size.width, spriteNode.size.height / 2 + groundTextureHeight * 2)
+            spriteNode.runAction(action)
+            self.addChild(spriteNode)
+        }
+    }
+    
+    func animateContinuousTextures(texture: SKTexture, period: NSTimeInterval) -> SKAction {
+        let animateInitialSprite = SKAction.moveByX(-texture.size().width * 2.0, y: 0.0, duration: period * Double(texture.size().width) * 2)
+        let animateSubsequentSprite = SKAction.moveByX(texture.size().width * 2.0, y: 0.0, duration: 0)
+        let animateSpriteForever = SKAction.repeatActionForever(SKAction.sequence([animateInitialSprite, animateSubsequentSprite]))
+        return animateSpriteForever
+    }
+
 }
