13b92786010ef859f98a7675211a10626329adc0
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-08-19 11:43:19 +0700

    Added physics attribute to birdie.

--- Flappy Birdie/GameScene.swift
+++ Flappy Birdie/GameScene.swift
@@ -44,18 +44,27 @@ class GameScene: SKScene {
         // MARK: Birdie
         //*************
         
+        // textures
         let birdieTexture1 = SKTexture(imageNamed: "birdie1")
         birdieTexture1.filteringMode = SKTextureFilteringMode.Nearest
         let birdieTexture2 = SKTexture(imageNamed: "birdie2")
         birdieTexture2.filteringMode = SKTextureFilteringMode.Nearest
         
+        // flapping animation
         let birdieFlappingAnimation = SKAction.animateWithTextures([birdieTexture1, birdieTexture2], timePerFrame: 0.2)
         let repeatBirdieFlappingAnimationForever = SKAction.repeatActionForever(birdieFlappingAnimation)
         
+        // instantiation
         self.birdie = SKSpriteNode(texture: birdieTexture1)
         self.birdie.setScale(self.spriteSizeScale)
         self.birdie.position = CGPointMake(self.frame.size.width / 2.5, CGRectGetMidY(self.frame))
         self.birdie.runAction(repeatBirdieFlappingAnimationForever)
+        
+        // physics
+        self.birdie.physicsBody = SKPhysicsBody(circleOfRadius: self.birdie.size.height / 2)
+        self.birdie.physicsBody!.dynamic = true
+        self.birdie.physicsBody!.allowsRotation = false
+        
         self.addChild(self.birdie)
         
         //*************
@@ -63,7 +72,6 @@ class GameScene: SKScene {
         //*************
         
         let groundTexture = SKTexture(imageNamed: "ground")
-        groundTexture.filteringMode = SKTextureFilteringMode.Nearest
         self.generateContinuousSpriteNodes(groundTexture, action: self.animateContinuousTextures(groundTexture, period: 0.02))
         
         //**************
@@ -71,7 +79,6 @@ class GameScene: SKScene {
         //**************
         
         let skylineTexture = SKTexture(imageNamed: "skyline")
-        skylineTexture.filteringMode = SKTextureFilteringMode.Nearest
         self.generateContinuousSpriteNodes(skylineTexture, action: self.animateContinuousTextures(skylineTexture, period: 0.1), groundTextureHeight: groundTexture.size().height)
     }
     
@@ -96,6 +103,7 @@ class GameScene: SKScene {
         print(2 + Int(self.frame.size.width / (texture.size().width * 2)))
         print("---")
         **/
+        texture.filteringMode = SKTextureFilteringMode.Nearest
         for index in 0 ..< 2 + Int(self.frame.size.width / (texture.size().width * 2)) {
             let spriteNode = SKSpriteNode(texture: texture)
             spriteNode.setScale(self.spriteSizeScale)
@@ -106,10 +114,10 @@ class GameScene: SKScene {
     }
     
     func animateContinuousTextures(texture: SKTexture, period: NSTimeInterval) -> SKAction {
+        print(texture.description)
         let animateInitialSprite = SKAction.moveByX(-texture.size().width * 2.0, y: 0.0, duration: period * Double(texture.size().width) * 2)
         let animateSubsequentSprite = SKAction.moveByX(texture.size().width * 2.0, y: 0.0, duration: 0)
         let animateSpriteForever = SKAction.repeatActionForever(SKAction.sequence([animateInitialSprite, animateSubsequentSprite]))
         return animateSpriteForever
     }
-
 }
